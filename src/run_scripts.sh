f5dcdd2ea7cdb62f0d22e71329c813cbc0a1c749
Ashish Thomas Mathew <ashishmathew0297@gmail.com>
2023-12-07 11:30:24 -0500

    Testing failure on certain images

--- src/run_scripts.sh
+++ src/run_scripts.sh
@@ -7,8 +7,25 @@ BOLD=$(tput bold)
 
 g++ -Wall -std=c++11 -o steinberg main.cpp $(pkg-config --cflags --libs opencv4)
 
-printf "%s%sPlease Wait:%s Generating image ...\n" "${BOLD}" "${YELLOW}" "${NORMAL}"
-time ./steinberg
-printf "%s%sSuccess:%s Image successfully generated!\n\n" "${BOLD}" "${GREEN}" "${NORMAL}"
+printf "%s%sPlease Wait:%s Generating images from ../input ...\n" "${BOLD}" "${YELLOW}" "${NORMAL}"
+
+# time ./steinberg "elden_ring.jpg" 0
+# time ./steinberg "elden_ring.jpg" 1
+
+for filename in ../input/*.*; do
+  printf "Generating quantized and dithered results and time readings for %s\n" "$(basename "${filename}")";
+  # Removing files if they already exist
+  rm ../time_measurements/"quantizing_$(basename "${filename%.*}").dat"
+  rm ../time_measurements/"dithering_$(basename "${filename%.*}").dat"
+
+  TIMEFORMAT='%R'
+  { time ./steinberg "$(basename "${filename}")" 0 1; } 2>> ../time_measurements/"quantizing_$(basename "${filename%.*}").dat"
+  { time ./steinberg "$(basename "${filename}")" 1 1; } 2>> ../time_measurements/"dithering_$(basename "${filename%.*}").dat"
+done
+
+printf "%s%sExecution finished:%s Please check the output and time_readings folders for the results\n\n" "${BOLD}" "${GREEN}" "${NORMAL}"
+
+# Uncomment this code for memory checks
+# valgrind --tool=memcheck ./steinberg "elden_ring.jpg" 1 2> "../debug_logs/large_image_valgrind.log"
 
 rm steinberg

c6271b1797316ff2e58149b9d03a6b1160315275
Ashish Thomas Mathew <ashishmathew0297@gmail.com>
2023-12-11 19:45:06 -0500

    changes to python code and removal of few images

--- src/error_plots_and_calculations.py
+++ src/error_plots_and_calculations.py
@@ -5,13 +5,13 @@ from matplotlib import rc
 
 rc('text', usetex=True)
 
-small_image_serial_by_factor = np.loadtxt('../time_measurements/serial_dithering_time_small.dat')
-large_image_serial_by_factor = np.loadtxt('../time_measurements/serial_dithering_time_large.dat')
+serial_dithering_time_small = np.loadtxt('../time_measurements/serial_dithering_time_small.dat')
+serial_dithering_time_large = np.loadtxt('../time_measurements/serial_dithering_time_large.dat')
 
-x_serial = small_image_serial_by_factor[:,0]
+x_serial = serial_dithering_time_small[:,0]
 
-y_serial_small = small_image_serial_by_factor[:,1:]
-y_serial_large = large_image_serial_by_factor[:,1:]
+y_serial_small = serial_dithering_time_small[:,1:]
+y_serial_large = serial_dithering_time_large[:,1:]
 
 
 fig_1, axis_1 = plt.subplots(1,2, figsize=(10, 4))
@@ -33,40 +33,48 @@ axis_1[1].legend(loc=2)
 plt.savefig("../output/graphs/serial_time_readings.png", dpi=200)
 
 
+parallel_time_data_large = np.loadtxt('../time_measurements/parallel_dithering_time_large.dat')
+parallel_time_data_small = np.loadtxt('../time_measurements/parallel_dithering_time_small.dat')
+
+number_of_threads = parallel_time_data_large[:,0]
+
+strong_scaling_times_large = np.mean(parallel_time_data_large[:,1:], axis=1)
+strong_scaling_times_small = np.mean(parallel_time_data_small[:,1:], axis=1)
+
+# Calculating Speedup = T1/Tp
+strong_scaling_speedup_large = np.full_like(strong_scaling_times_large, strong_scaling_times_large[0])/strong_scaling_times_large
+strong_scaling_speedup_small = np.full_like(strong_scaling_times_small, strong_scaling_times_small[0])/strong_scaling_times_small
+
+print(strong_scaling_speedup_large)
+print(strong_scaling_speedup_small)
+
+fig_2, axis_2 = plt.subplots()
+axis_3 = axis_2.twinx()
+ln1 = axis_2.plot(number_of_threads, strong_scaling_speedup_large, color='#b58900', linestyle='-', label=r'$\mathbf{Speedup\;for\;a\;large\;image}$')
+ln2 = axis_3.plot(number_of_threads, strong_scaling_speedup_small, color='#2aa198', linestyle='-', label=r'$\mathbf{Speedup\;for\;a\;small\;image}$')
+ln3 = axis_2.plot(number_of_threads[0:2], number_of_threads[0:2], color='#dc322f', linestyle='-', label=r'$\mathbf{Ideal\;Speedup}$')
+axis_2.set_xlabel('$\mathbf{Number\;of\;Processors\;(p)}$', fontdict={'color':'#000000'})
+axis_2.set_ylabel(r'$\mathbf{Speedup}$', fontdict={'color':'#b58900'})
+axis_3.set_ylabel(r'$\mathbf{Speedup}$', fontdict={'color':'#2aa198'})
+axis_2.set_title(r'$\mathbf{Strong\;Scaling\;Test\;Results\;for\;a\;Large\;and\;Small\;Image}$', fontdict={'color':'#000000'})
+lns = ln1+ln2+ln3
+labs = [l.get_label() for l in lns]
+axis_2.legend(lns, labs, loc=4)
+plt.savefig("../output/graphs/strong_scaling_test_plots.png", dpi=200,bbox_inches='tight')
+
 
 # fig_2 = plt.figure(2)
 # plt.grid(visible=True)
-# plt.loglog(x_parallel, y_parallel, color='#2aa198', linestyle='-', label=r'$\mathbf{Error\;Reading\; \langle E(N) \rangle = |2^{10} - \hat{I}(N)| }$')
+# plt.plot(number_of_threads, strong_scaling_speedup_large, color='#b58900', linestyle='-', label=r'$\mathbf{Error\;Reading\; \langle E(N) \rangle = |2^{10} - \hat{I}(N)| }$')
+# plt.plot(number_of_threads, strong_scaling_speedup_small, color='#b58900', linestyle='-', label=r'$\mathbf{Error\;Reading\; \langle E(N) \rangle = |2^{10} - \hat{I}(N)| }$')
 # plt.plot(x_parallel, 10**log_y_parallel_best_fit, color='#b58900', linestyle='-.', label=r'$\mathbf{Best\;Fit}$', markersize=4)
-# plt.loglog(x_parallel, expected_convergence_rate_parallel, color='#dc322f', linestyle='-', label=r'$N^{-1/2}$', markersize=4)
 # plt.xlabel('$\mathbf{N}$', fontdict={'color':'#000000'})
 # plt.ylabel('$\mathbf{Absolute\;Error}$', fontdict={'color':'#000000'})
 # plt.title('$\mathbf{Absolute\;Error\;vs\;N}$', fontdict={'color':'#000000'})
 # plt.legend(loc=3)
 # plt.savefig("./outputs/parallel_error_plot.png", dpi=300)
 
-# rosenbrock_data = np.loadtxt('./generated_data/rosenbrock_function_points.dat')
-# x_rosen = rosenbrock_data[:,0]
-# y_rosen = rosenbrock_data[:,1]
-# z_rosen = rosenbrock_data[:,2]
 
-# x_mesh, y_mesh = np.meshgrid(np.arange(-1.0, 1.0, 0.2), np.arange(-1.0, 1.0, 0.2))
-
-# z_mesh = rosen([x_mesh, y_mesh])
-
-# fig_3 = plt.figure(3)
-# ax = plt.axes(projection="3d")
-# generated_points = ax.scatter(x_rosen,y_rosen,z_rosen, s=3, marker="d", color="#000000", alpha=0.8)
-# surf = ax.plot_surface(x_mesh, y_mesh, z_mesh, cmap=cm.hsv, linewidth=1)
-# ax.set_xlabel('$\mathbf{x_{1}}$')
-# ax.set_ylabel('$\mathbf{x_{2}}$')
-# ax.set_zlabel('$\mathbf{Generated\;x_{3}}$')
-# ax.xaxis.set_rotate_label(False)
-# ax.yaxis.set_rotate_label(False)
-# ax.view_init(30,60)
-# fig_2.colorbar(surf, shrink=0.5, location="left")
-# # plt.show()
-# plt.savefig("./outputs/rosenbrock.png", dpi=300)
 
 
 # strong_scaling_test_data = np.loadtxt('./generated_data/strong_scaling_test_anvil.dat')
@@ -100,66 +108,3 @@ plt.savefig("../output/graphs/serial_time_readings.png", dpi=200)
 # axis[1].set_title(r'$\mathbf{Weak\;Scaling\;Test\;Results}$')
 # axis[1].legend(loc=3)
 # plt.savefig("./outputs/scaling_test_plots.png", dpi=300)
-
-# integral_data = np.loadtxt('./generated_data/rosenbrock_integral_data.dat')
-# N = integral_data[0,0::2]
-# I_n = np.mean(np.transpose(integral_data[:,1::2]), axis=1)
-
-# fig_5 = plt.figure(5)
-# plt.grid(visible=True)
-# plt.loglog(N, I_n, base=4, color='#2aa198', linestyle='-', label=r'$\mathbf{Integral\;Value}$')
-# plt.xlabel('$\mathbf{Number\;of\;Points\;(N)}$', fontdict={'color':'#000000'})
-# plt.ylabel(r'$\mathbf{Calculated\;Integral\;Value\;\;\hat{I}(N)}$', fontdict={'color':'#000000'})
-# plt.title(r'$\mathbf{Number\;of\;Threads\;vs\;Speedup\;\frac{T_{1}}{T_{P}}}$', fontdict={'color':'#000000'})
-# plt.legend(loc=4)
-# plt.savefig("./outputs/integral_vs_N.png", dpi=300)
-
-# serial_error_estimator_data = np.loadtxt('./generated_data/serial_error_estimator.dat')
-# parallel_error_estimator_data = np.loadtxt('./generated_data/parallel_error_estimator.dat')
-
-# N = serial_error_estimator_data[0,0::3]
-
-# serial_integral_values = serial_error_estimator_data[:,1::3]
-# parallel_integral_values = parallel_error_estimator_data[:,1::3]
-
-# serial_estimated_errors = np.mean(np.transpose(serial_error_estimator_data[:,2::3]), axis=1)
-# parallel_estimated_errors = np.mean(np.transpose(parallel_error_estimator_data[:,2::3]), axis=1)
-
-# serial_actual_errors = np.mean(np.transpose(np.absolute(1024 - serial_integral_values)), axis=1)
-# parallel_actual_errors = np.mean(np.transpose(np.absolute(1024 - parallel_integral_values)), axis=1)
-
-# log_N = np.emath.logn(4, N)
-
-# serial_log_E = np.emath.logn(4, serial_estimated_errors)
-# parallel_log_E = np.emath.logn(4, parallel_estimated_errors)
-
-# A_serial,B_serial = np.polyfit(log_N, serial_log_E, 1)
-# A_parallel,B_parallel = np.polyfit(log_N, parallel_log_E, 1)
-
-# print("Rate of descent of the Series error estimator is = ", A_serial)
-# print("Rate of descent of the Parallel error estimator is = ", A_parallel)
-
-# serial_log_E_best_fit = A_serial*log_N + B_serial
-# parallel_log_E_best_fit = A_parallel*log_N + B_parallel
-
-# fig_6, axis_1 = plt.subplots(1,2, figsize=(10, 4))
-# fig_6.tight_layout(pad=4.0)
-# fig_6.subplots_adjust(wspace=0.275)
-# axis_1[0].grid(visible=True)
-# axis_1[0].loglog(N, serial_estimated_errors, base=4, color='#2aa198', linestyle='-', linewidth=2, label=r'$\mathbf{Estimated\;Error}$')
-# axis_1[0].loglog(N, serial_actual_errors, base=4, color='#b58900', linestyle='-', linewidth=2, label=r'$\mathbf{Actual\;Error}$')
-# axis_1[0].plot(N, 4**serial_log_E_best_fit, color='#dc322f', linestyle='--', linewidth=2, label=r'$\mathbf{Best\;Fit}$', markersize=4)
-# axis_1[0].set_xlabel('$\mathbf{N}$', fontdict={'color':'#000000'})
-# axis_1[0].set_ylabel('$\mathbf{Absolute\;Error}$', fontdict={'color':'#000000'})
-# axis_1[0].set_title('$\mathbf{Error\;Estimator\;(Serial\;Code)}$', fontdict={'color':'#000000'})
-# axis_1[0].legend(loc=3)
-
-# axis_1[1].grid(visible=True)
-# axis_1[1].loglog(N, parallel_estimated_errors, base=4, color='#2aa198', linestyle='-', linewidth=2, label=r'$\mathbf{Estimated\;Error}$')
-# axis_1[1].loglog(N, parallel_actual_errors, base=4, color='#b58900', linestyle='-', linewidth=2, label=r'$\mathbf{Actual\;Error}$')
-# axis_1[1].plot(N, 4**parallel_log_E_best_fit, color='#dc322f', linestyle='--', linewidth=2, label=r'$\mathbf{Best\;Fit}$', markersize=4)
-# axis_1[1].set_xlabel('$\mathbf{N}$', fontdict={'color':'#000000'})
-# axis_1[1].set_ylabel('$\mathbf{Absolute\;Error}$', fontdict={'color':'#000000'})
-# axis_1[1].set_title('$\mathbf{Error\;Estimator\;(Parallel\;Code)}$', fontdict={'color':'#000000'})
-# axis_1[1].legend(loc=3)
-# plt.savefig("./outputs/error_estimator_plots.png", dpi=300)
